acc398c04152ba433ea077967dbe901efa8abff0
Timothy Odebunmi <dev.brymes@gmail.com>
2023-02-17 09:16:10 +0100

    Feat: Send Notification to discord for each Whale

--- src/alchemy/alchemy.service.ts
+++ src/alchemy/alchemy.service.ts
@@ -1,24 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { NotifyPayloadDTO } from './dto/create-alchemy.dto';
+import { SendNotification } from '../bot/sendMessage';
 
 @Injectable()
 export class AlchemyService {
   handleWebHook(notifyPayload: NotifyPayloadDTO) {
-    const bk = new Promise((resolve, reject) => {
+    const handler = new Promise((resolve, reject) => {
       resolve(() => {
-        console.log('Function Start');
-        //Call EtherScan API
-        //Send Message
-        setTimeout(() => {
-          console.log(notifyPayload);
-        }, 10000);
-        console.log('Function END');
+        const activities = notifyPayload.event.activity;
+        for (let i = 0; i < activities.length; i++) {
+          SendNotification(activities[i]);
+        }
       });
     });
 
-    console.log('MAIN START');
-    bk.then((f: any) => f());
-    console.log('MAIN end');
+    handler.then((fn: any) => fn());
 
     return;
   }
